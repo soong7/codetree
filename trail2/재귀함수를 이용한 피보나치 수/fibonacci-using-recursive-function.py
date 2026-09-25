8b08690a673c4cf9f744b9a24476b84d89a56879
soong7 <seungah715@naver.com>
2026-09-26 00:12:43 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (75ms, 19MB)

---
+++ trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+# Please write your code here.
+
+"""
+Trail2
+Lesson 2. 값을 반환하는 재귀함수
+2026-09-26
+
+재귀함수를 이용한 피보나치 수
+"""
+
+def fun(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    return fun(n - 1) + fun(n - 2)
+
+print(fun(N))
