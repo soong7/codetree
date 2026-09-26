7c53d0837cd110bd3eea20e367d40eadd1c99f8c
soong7 <seungah715@naver.com>
2026-09-26 21:18:42 +0900

    [Passed] 재귀함수를 이용한 최댓값 (48ms, 16MB)

---
+++ trail2/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+"""
+Trail2
+Lesson 2. 값을 반환하는 재귀함수
+
+재귀함수를 이용한 최댓값
+"""
+
+"""
+잘 모르겠어서 해설 봄
+근데 재귀 안에서 max 쓸 거 같으면
+그냥 max(arr)하면 되지ㅋㅋ 황당하다
+"""
+
+print(max(arr))
